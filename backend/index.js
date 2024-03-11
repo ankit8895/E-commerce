37023ddfc0da06bec4e0c53229bc54a5c6821e24
ankit8895 <125978383+ankit8895@users.noreply.github.com>
2024-03-12 03:15:40 +0530

    update root handle route

--- backend/index.js
+++ backend/index.js
@@ -36,6 +36,10 @@ app.get('/api/config/googlepay', (req, res) =>
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
 app.use(notFound);
 
 app.use(errorHandler);
