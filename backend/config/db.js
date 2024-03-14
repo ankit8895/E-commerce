a6708affba6e88e1ad60c6d757901eeca33dd2c4
ankit8895 <125978383+ankit8895@users.noreply.github.com>
2024-03-14 05:33:43 +0530

    fix mongoDB connection issue

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       dbName: 'proshop',
-      bufferCommands: false,
+      bufferCommands: true,
     });
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
